fe845bec3b5f2f3a97d96d6f5453ad242d49a42c
David OKeefe <davidpokeefe3@gmail.com>
2016-07-31 13:33:24 -0400

    Add header and very basic header styling

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,6 +2,6 @@ feature "User visits homepage" do
   scenario "successfully" do
     visit root_path
 
-    expect(page).to have_css 'h1', text: 'Snapshot'
+    expect(page).to have_css 'h1', text: 'snapshot'
   end
 end
