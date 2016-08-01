26445f10777ec96bb9809fdf5bba91c1a61ee447
David OKeefe <davidpokeefe3@gmail.com>
2016-07-31 21:07:13 -0400

    Creates import logic for ImportCSVEntries

--- app/models/import_csv_entries.rb
+++ app/models/import_csv_entries.rb
@@ -6,5 +6,25 @@ class ImportCSVEntries
   validates :import_entry, presence: true
 
   def import
+    csv = CSV.foreach(import_entry, headers: true, header_converters: :symbol) do |row|
+      ActiveRecord::Base.transaction do
+        Entry.create!(
+          name: row[:description],
+          amount: row[:amount],
+          date: importable_date(row[:post_date]),
+          transaction_type: importable_transaction_type(row[:type])
+        )
+      end
+    end
+  rescue => error
+    self.errors.add("ImportCSV", "was not successful")
+  end
+
+  def importable_transaction_type(type)
+    TransactionType.find_or_create_by(name: type.downcase)
+  end
+
+  def importable_date(date)
+    Date.strptime(date, "%m/%d/%Y").iso8601
   end
 end
